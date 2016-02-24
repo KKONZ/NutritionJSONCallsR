bbfd05e56c8d6817d547a000ebbc9e9d27a51bc7
Karl Konz <HelloGitWorld@gmail.com>
2016-02-24 06:51:34 -0700

    Update JSONCall.R

--- JSONCall.R
+++ JSONCall.R
@@ -2,4 +2,5 @@ example JSON from USDA nutritional facts
 carrots <- fromJSON("http://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=b&format=json&api_key=DEMO_KEY") 
 carrots <- toJSON(carrots, pretty=TRUE) 
 carrotsN <-carrots$report$food$nutrients
-names(flatten(carrotsN))
+names <- names(flatten(carrotsN))
+carrots <- carrotsN[2:6, ]; rm(carrotsN)
